d021299bee4d7f614fddbb1bf3f6628868dcc3d8
lekoal <andlekoal@gmail.com>
2022-06-02 21:23:30 +0300

    Default dark theme added

--- app/src/main/java/com/example/geekbrainstranslator/view/MainActivity.kt
+++ app/src/main/java/com/example/geekbrainstranslator/view/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.geekbrainstranslator.view
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatDelegate
 import com.example.geekbrainstranslator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +13,9 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+        delegate.applyDayNight()
 
         if (savedInstanceState == null) {
             supportFragmentManager
