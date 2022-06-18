79f0ad2b4077b661bf74dd744aa2d11d6e36e65c
andlekoal <andlekoal@gmail.com>
2022-06-18 21:24:38 +0300

    WordData names fixing

--- data/src/main/java/com/example/data/entity/db/WordData.kt
+++ data/src/main/java/com/example/data/entity/db/WordData.kt
@@ -8,18 +8,28 @@ import androidx.room.PrimaryKey
 @Entity(indices = [Index("id", unique = true)])
 data class WordData(
     @field: PrimaryKey(autoGenerate = true)
-    @field:ColumnInfo(name = "index")
+    @field:ColumnInfo(name = INDEX_NAME)
     val index: Int = 0,
-    @field: ColumnInfo(name = "id")
+    @field: ColumnInfo(name = ID_NAME)
     val id: Int?,
-    @field: ColumnInfo(name = "text")
+    @field: ColumnInfo(name = TEXT_NAME)
     val text:String?,
-    @field: ColumnInfo(name = "imageUrl")
+    @field: ColumnInfo(name = IMAGE_URL_NAME)
     val imageUrl: List<String?>,
-    @field: ColumnInfo(name = "transcription")
+    @field: ColumnInfo(name = TRANSCRIPTION_NAME)
     val transcription: List<String?>,
-    @field: ColumnInfo(name = "translation")
+    @field: ColumnInfo(name = TRANSLATION_NAME)
     val translation: List<String?>,
-    @field: ColumnInfo(name = "partOfSpeechCode")
+    @field: ColumnInfo(name = PART_OF_SPEECH_NAME)
     val partOfSpeechCode: List<String?>
-)
+) {
+    companion object {
+        private const val INDEX_NAME = "index"
+        private const val ID_NAME = "id"
+        private const val TEXT_NAME = "text"
+        private const val IMAGE_URL_NAME = "imageUrl"
+        private const val TRANSCRIPTION_NAME = "transcription"
+        private const val TRANSLATION_NAME = "translation"
+        private const val PART_OF_SPEECH_NAME = "partOfSpeechCode"
+    }
+}
