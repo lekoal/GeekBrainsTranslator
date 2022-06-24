015ce49f32ad5a2ea50647800e34b489e34c1c34
lekoal <coreglus@gmail.com>
2022-06-24 11:13:30 +0300

    Splash screen with animation added

--- app/src/main/java/com/example/geekbrainstranslator/view/main/MainActivity.kt
+++ app/src/main/java/com/example/geekbrainstranslator/view/main/MainActivity.kt
@@ -1,8 +1,16 @@
 package com.example.geekbrainstranslator.view.main
 
+import android.animation.ObjectAnimator
+import android.os.Build
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AnticipateInterpolator
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
+import androidx.core.animation.doOnEnd
 import com.example.geekbrainstranslator.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +22,8 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        setSplashScreen()
+
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         delegate.applyDayNight()
 
@@ -22,6 +32,51 @@ class MainActivity : AppCompatActivity() {
                 .beginTransaction()
                 .add(binding.mainActivityContainer.id, MainTranslationFragment.newInstance())
                 .commit()
+        }
+    }
+
+    private fun setSplashScreen() {
+        var isHideSplashScreen = false
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            setSplashScreenAnimation()
+        }
+
+        object : CountDownTimer(1000, 1000) {
+            override fun onTick(millisUntilFinished: Long) {}
+            override fun onFinish() {
+                isHideSplashScreen = true
+            }
+        }.start()
+
+        val content: View = findViewById(android.R.id.content)
+        content.viewTreeObserver.addOnPreDrawListener(
+            object : ViewTreeObserver.OnPreDrawListener {
+                override fun onPreDraw(): Boolean {
+                    return if (isHideSplashScreen) {
+                        content.viewTreeObserver.removeOnPreDrawListener(this)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            }
+        )
+    }
+
+    @RequiresApi(31)
+    private fun setSplashScreenAnimation() {
+        splashScreen.setOnExitAnimationListener { view ->
+            val slideLeft = ObjectAnimator.ofFloat(
+                view,
+                View.TRANSLATION_Y,
+                0f,
+                view.height.toFloat()
+            )
+            slideLeft.interpolator = AnticipateInterpolator()
+            slideLeft.duration = 1000
+            slideLeft.doOnEnd { view.remove() }
+            slideLeft.start()
         }
     }
 }
