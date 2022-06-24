aab40fd41aaf5b7f3d94477fbb38d9e7ea4260ec
lekoal <coreglus@gmail.com>
2022-06-24 11:19:20 +0300

    Image bluring for word's detail photo is added

--- app/src/main/java/com/example/geekbrainstranslator/view/description/DescriptionWordViewHolder.kt
+++ app/src/main/java/com/example/geekbrainstranslator/view/description/DescriptionWordViewHolder.kt
@@ -1,8 +1,11 @@
 package com.example.geekbrainstranslator.view.description
 
+import android.graphics.RenderEffect
+import android.graphics.Shader
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
+import androidx.annotation.RequiresApi
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.data.entity.db.WordDataDetails
@@ -14,12 +17,20 @@ class DescriptionWordViewHolder(itemView: View) : RecyclerView.ViewHolder(itemVi
     private val translation: TextView = itemView.findViewById(R.id.entity_translation)
     private val partOfSpeech: TextView = itemView.findViewById(R.id.entity_part_of_speech_code)
 
+    private val blurEffect = RenderEffect.createBlurEffect(16f, 16f, Shader.TileMode.MIRROR)
+
     fun bind(item: WordDataDetails) {
+        imageBlur()
         Glide.with(itemView)
             .load("https:${item.imageUrl}")
             .into(image)
         transcription.text = item.transcription
         translation.text = item.translation
         partOfSpeech.text = item.partOfSpeechCode
+    }
+
+    @RequiresApi(31)
+    private fun imageBlur() {
+        image.setRenderEffect(blurEffect)
     }
 }
